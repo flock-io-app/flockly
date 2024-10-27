86acddb12ff9029a88bb35164c2f2d98e88d3d51
Lycrat <Panya_R@outlook.com>
2024-10-27 09:31:44 +0000

    Fix finding duplicate data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,6 +145,8 @@ export default function Home() {
 
   const parseData = (data: any) => {
     const dat = data["nodes"];
+    TRAINER_NODES = [];
+    VALIDATOR_NODES = [];
 
     Object.entries(dat).forEach(([address, task], index) => {
       const [parsedNode, trainer, validator] = parseNodeData(
